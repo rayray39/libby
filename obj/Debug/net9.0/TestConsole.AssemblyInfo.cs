857c77997086e892f03fad13287d4ad40f479ba3
rayray39 <raynerlimfy@gmail.com>
2024-12-03 22:07:24 +0800

    Place .cs files into src folder

--- obj/Debug/net9.0/TestConsole.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b95acc8019be339cdabeefc6637ea929b860eba")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
