56375e112770b04b2355069c7a6c106e5541c3e2
rayray39 <raynerlimfy@gmail.com>
2024-12-05 14:21:41 +0800

    Correct spelling error for greet

--- src/User.cs
+++ src/User.cs
@@ -3,13 +3,13 @@ class User
     // public string name;    // name of user
 
     // property name, readonly
-    public string Name{ get; set; }
+    public string Name{ get; init; }
      
     public User(string name) {
         Name = name;
     }
 
     public void greet() {
-        Console.WriteLine("You ae now logged in as {0}\n", Name );
+        Console.WriteLine("You are now logged in as {0}\n", Name );
     }
 }
